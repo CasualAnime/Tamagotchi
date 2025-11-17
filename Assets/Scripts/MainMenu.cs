28ad8060b22965767f30e8d26fbaca7cfad21846
CasualAnime <Naruto170612@gmail.com>
2025-11-16 22:11:39 -0500

    Fixed Resolution & Added in Basic Main Menu
    Added in a basic main menu that contains just the play button. I also fixed up the resolution of the canva so that the pivots of the status bars & buttons work properly.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,11 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void OnButtonClick()
+    {
+        SceneManager.LoadScene("Main Game");
+    }
+}
